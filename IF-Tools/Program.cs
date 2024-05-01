3d6d75db1a6f97607fd119b3f10bcde31a4ed219
Hleb Zhynko <gleb.zhinko@protonmail.com>
2024-05-01 14:51:19 -0500

    fix controller error on startup

--- IF-Tools/Program.cs
+++ IF-Tools/Program.cs
@@ -7,6 +7,7 @@ DotEnv.Load(new DotEnvOptions(true, new [] {"../.env"}));
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
